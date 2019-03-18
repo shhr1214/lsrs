a1f332f17065d6ec2a9e3732cf967ca08900a8bf
shhr1214 <souhei0079@gmail.com>
2019-03-18 23:29:14 +0900

    ls -l みたいな挙動を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let dir = ".";
+
+    for entry in fs::read_dir(dir).unwrap() {
+        let entry = entry.unwrap();
+        println!(
+            "{}",
+            entry
+                .path()
+                .as_path()
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+        );
+    }
 }
